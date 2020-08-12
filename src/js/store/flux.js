046d438e2fc08c0bd48820a5c52e00d19d21fb72
moldovanjason <moldovanjason@gmail.com>
2020-08-12 21:25:30 +0000

    4

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,25 +1,15 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			allContacts: ["Jason", "pedro", "Juan"]
+			allContacts: []
 		},
 		actions: {
-            // addContacts(...args) {
-            //     const store = getStore();
-            //     const newObjArr = args.reduce((obj, value) => {
-            //     const newObj = {};
-            //         newObj[value] = value
-            //         return {...obj, newObj}});
-            //     const newState = store.allContact.concat(newObjArr)
-            //     setStore({allContact: newState})
-            // },
-
 			addContacts: (name, address, number, email) => {
 				const store = getStore();
 				const newContact = [{ name, address, number, email }];
 				const finContact = store.allContacts.concat(newContact);
 				setStore({ allContacts: finContact });
-            },
+			},
 
 			deleteContacts: indexDel => {
 				const store = getStore();
@@ -30,9 +20,22 @@ const getState = ({ getStore, setStore }) => {
 			editContact: (name, address, phone, email, indexDel) => {
 				const store = getStore();
 				const modContact = { name, address, phone, email };
-				const updatedContact = store.allContacts.map((value, index) => index === indexDel ? modContact : value);
+				const updatedContact = store.allContacts.map((value, index) =>
+					index === indexDel ? modContact : value
+				);
 				setStore({ allContacts: updatedContact });
 			}
+
+			// addContacts(...args) {
+			//     const store = getStore();
+			//     const newObjArr = args.reduce((obj, value) => {
+			//     const newObj = {};
+			//         newObj[value] = value
+			//         return {...obj, newObj}});
+			//     const newState = store.allContact.concat(newObjArr)
+			//     setStore({allContact: newState})
+			// },
+
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
--- src/js/views/AddContact.js
+++ src/js/views/AddContact.js
@@ -1,36 +1,80 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
+import { Context } from "../store/appContext";
 
 export const AddContact = () => {
+	const [state, setState] = useState({
+		name: null,
+		email: null,
+		phone: null,
+		address: null
+	});
 	return (
 		<div className="container">
-			<div>
-				<h1 className="text-center mt-5">Add a new contact</h1>
-				<form>
-					<div className="form-group">
-						<label>Full Name</label>
-						<input type="text" className="form-control" placeholder="Full Name" />
+			<Context.Consumer>
+				{({ store, actions }) => (
+					<div>
+						<h1 className="text-center mt-5">Add a new contact</h1>
+						<form>
+							<div className="form-group">
+								<label>Full Name</label>
+								<input
+									onChange={event => {
+										setState({ ...state, name: event.target.value });
+									}}
+									type="text"
+									className="form-control"
+									placeholder="Full Name"
+								/>
+							</div>
+							<div className="form-group">
+								<label>Email</label>
+								<input
+									onChange={event => {
+										setState({ ...state, email: event.target.value });
+									}}
+									type="email"
+									className="form-control"
+									placeholder="Enter email"
+								/>
+							</div>
+							<div className="form-group">
+								<label>Phone</label>
+								<input
+									onChange={event => {
+										setState({ ...state, phone: event.target.value });
+									}}
+									type="phone"
+									className="form-control"
+									placeholder="Enter phone"
+								/>
+							</div>
+							<div className="form-group">
+								<label>Address</label>
+								<input
+									onChange={event => {
+										setState({ ...state, address: event.target.value });
+									}}
+									type="text"
+									className="form-control"
+									placeholder="Enter address"
+								/>
+							</div>
+							<button
+								onClick={() =>
+									actions.addContacts(state.name, state.address, state.number, state.email)
+								}
+								type="button"
+								className="btn btn-primary form-control">
+								save
+							</button>
+							<Link className="mt-3 w-100 text-center" to="/">
+								or get back to contacts
+							</Link>
+						</form>
 					</div>
-					<div className="form-group">
-						<label>Email</label>
-						<input type="email" className="form-control" placeholder="Enter email" />
-					</div>
-					<div className="form-group">
-						<label>Phone</label>
-						<input type="phone" className="form-control" placeholder="Enter phone" />
-					</div>
-					<div className="form-group">
-						<label>Address</label>
-						<input type="text" className="form-control" placeholder="Enter address" />
-					</div>
-					<button type="button" className="btn btn-primary form-control">
-						save
-					</button>
-					<Link className="mt-3 w-100 text-center" to="/">
-						or get back to contacts
-					</Link>
-				</form>
-			</div>
+				)}
+			</Context.Consumer>
 		</div>
 	);
 };
