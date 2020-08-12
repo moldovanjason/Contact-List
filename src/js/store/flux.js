bd441a95c383e4b5f3614af2b8e55dc4fde718b6
moldovanjason <moldovanjason@gmail.com>
2020-08-12 16:45:00 +0000

    3

--- src/js/store/flux.js
+++ src/js/store/flux.js
@@ -1,12 +1,23 @@
 const getState = ({ getStore, setStore }) => {
 	return {
 		store: {
-			//Your data structures, A.K.A Entities
+			allContacts: ["Jason", "pedro", "Juan"]
 		},
 		actions: {
-            addContacts = () => {},
-            deleteContacts = () => {},
-            editContacts = () => {}
+			addContacts: (name, address, number, email) => {
+				const store = getStore();
+				const newContact = [{ name, address, number, email }];
+				const finContact = store.allContacts.concat(newContact);
+				setStore({ allContacts: finContact });
+			},
+
+			deleteContacts: indexDel => {
+				const store = getStore();
+				const newArr = store.allContacts.filter((value, index) => index !== indexDel);
+				setStore({ allContacts: newArr });
+			},
+
+			editContacts: () => {}
 			//(Arrow) Functions that update the Store
 			// Remember to use the scope: scope.state.store & scope.setState()
 		}
