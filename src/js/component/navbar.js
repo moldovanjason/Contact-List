d35358b91ef745d03c3ab01e57d3d2d2dff3989e
moldovanjason <moldovanjason@gmail.com>
2020-10-28 02:49:24 +0000

    added navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,19 +1,14 @@
 import React from "react";
 import { Link } from "react-router-dom";
 
-export class Navbar extends React.Component {
+class Navbar extends React.Component {
 	render() {
 		return (
-			<nav className="navbar navbar-light bg-light mb-3">
-				<Link to="/">
-					<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-				</Link>
-				<div className="ml-auto">
-					<Link to="/demo">
-						<button className="btn btn-primary">Check the Context in action</button>
-					</Link>
-				</div>
+			<nav className="navbar mb-3">
+				<h2 className="text">Contact List</h2>
 			</nav>
 		);
 	}
 }
+
+export default Navbar;
--- src/js/views/Contacts.js
+++ src/js/views/Contacts.js
@@ -2,10 +2,12 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import { Context } from "../store/appContext";
 import ContactCard from "../component/ContactCard.js";
+import Navbar from "../component/navbar";
 
 export const Contacts = () => {
 	return (
 		<div className="container">
+			<Navbar />
 			<Context.Consumer>
 				{({ store, actions }) => (
 					<div>
