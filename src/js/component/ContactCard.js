b4e515581dc2241c382cddbce483359acf716fdf
Jason Moldovan <moldovanjason@gmail.com>
2020-11-15 22:18:30 -0500

    context.consumer

--- src/js/component/ContactCard.js
+++ src/js/component/ContactCard.js
@@ -8,51 +8,51 @@ const ContactCard = props => {
 	console.log(props);
 	return (
 		<li className="list-group-item">
-			{/* <Context.Consumer> */}
-				{({ store, actions }) => (
-					<div className="row w-100">
-						{/* <div className="col-12 col-sm-6 col-md-3 px-0">
+			<Context.Consumer>
+			{({ store, actions }) => (
+				<div className="row w-100">
+					{/* <div className="col-12 col-sm-6 col-md-3 px-0">
 							<img
 								src={MikePhoto}
 								alt="Insert Photo"
 								className="rounded-circle mx-auto d-block img-fluid"
 							/>
 						</div> */}
-						<div className="col-12 col-6 text-center text-sm-left">
-							<div className="editdelbutton float-right">
-								<button className="btn" onClick={() => props.history.push(`/edit/${props.id}`)}>
-									<i className="fas fa-pencil-alt mr-3" />
-								</button>
-								<button className="btn" onClick={() => actions.deleteContacts(props.id)}>
-									<i className="fas fa-trash-alt" />
-								</button>
-							</div>
-							<label className="name lead">{props.name}</label>
-							<br />
-							<i className="fas fa-map-marker-alt text-muted mr-3" />
-							<span className="text-muted">{props.address}</span>
-							<br />
-							<span
-								className="fa fa-phone fa-fw text-muted mr-3"
-								data-toggle="tooltip"
-								title=""
-								data-original-title="(870) 288-414*"
-							/>
-							<span className="text-muted small">{props.phone}</span>
-							<br />
-							<span
-								className="fa fa-envelope fa-fw text-muted mr-3"
-								data-toggle="tooltip"
-								data-original-title=""
-								title=""
-							/>
-							<span className="text-muted small text-truncate">{props.email}</span>
-							<br />
-							<span className="text-muted small text-truncate">{props.stage}</span>
+					<div className="col-12 col-6 text-center text-sm-left">
+						<div className="editdelbutton float-right">
+							<button className="btn" onClick={() => props.history.push(`/edit/${props.id}`)}>
+								<i className="fas fa-pencil-alt mr-3" />
+							</button>
+							<button className="btn" onClick={() => actions.deleteContacts(props.id)}>
+								<i className="fas fa-trash-alt" />
+							</button>
 						</div>
+						<label className="name lead">{props.name}</label>
+						<br />
+						<i className="fas fa-map-marker-alt text-muted mr-3" />
+						<span className="text-muted">{props.address}</span>
+						<br />
+						<span
+							className="fa fa-phone fa-fw text-muted mr-3"
+							data-toggle="tooltip"
+							title=""
+							data-original-title="(870) 288-414*"
+						/>
+						<span className="text-muted small">{props.phone}</span>
+						<br />
+						<span
+							className="fa fa-envelope fa-fw text-muted mr-3"
+							data-toggle="tooltip"
+							data-original-title=""
+							title=""
+						/>
+						<span className="text-muted small text-truncate">{props.email}</span>
+						<br />
+						<span className="text-muted small text-truncate">{props.stage}</span>
 					</div>
-				)}
-			{/* </Context.Consumer> */}
+				</div>
+			)}
+			</Context.Consumer>
 		</li>
 	);
 };
